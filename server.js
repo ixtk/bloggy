08800280be1c994fae1db4346da66d1115309336
ixtk <ikhcamot@gmail.com>
2025-08-30 14:29:51 +0400

    Add basic templates

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ const app = express()
 
 app.set("view engine", "ejs")
 
+const blogPosts = [
+  {
+    name: "First Post",
+    text: "This is the first post",
+    liked: true
+  },
+  {
+    name: "Second Post",
+    text: "This is the second post",
+    liked: false
+  },
+  {
+    name: "Third Post",
+    text: "This is the third post",
+    liked: true
+  }
+]
+
 app.get("/", function (request, response) {
-  response.render("index", { name: "Emmet" })
+  response.render("index", {
+    posts: blogPosts
+  })
 })
 
 app.listen(3000)
