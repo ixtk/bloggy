2323ee34cda90853ca6a2222abc6d0cfcb0c10fc
ixtk <ikhcamot@gmail.com>
2025-09-13 14:03:34 +0400

    Add dynamic post endpoint

--- server.js
+++ server.js
@@ -1,35 +1,24 @@
 const express = require("express")
+const blogPosts = require('./posts.json')
 
 const app = express()
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-const blogPosts = [
-  {
-    id: 1,
-    name: "First Post",
-    text: "This is the first post",
-    liked: true
-  },
-  {
-    id: 2,
-    name: "Second Post",
-    text: "This is the second post",
-    liked: false
-  },
-  {
-    id: 3,
-    name: "Third Post",
-    text: "This is the third post",
-    liked: true
-  }
-]
-
 app.get("/", function (request, response) {
   response.render("index", { posts: blogPosts })
 })
 
+app.get("/posts/:postId", function (request, response) {
+  const postId = request.params.postId
+
+  console.log('User visited post', postId)
+
+  response.render("post", { post: blogPosts[postId - 1] })
+})
+
+/*
 app.get("/posts/1", function (request, response) {
   response.render("post", { post: blogPosts[0] })
 })
@@ -41,6 +30,7 @@ app.get("/posts/2", function (request, response) {
 app.get("/posts/3", function (request, response) {
   response.render("post", { post: blogPosts[2] })
 })
+*/
 
 // localhost:3000
 app.listen(3000)
