135e2e13f4ba45e2c04fbcaac0ab5f40d98ff206
ixtk <ikhcamot@gmail.com>
2025-09-17 12:04:10 +0400

    Use MongoDB as the information source

--- server.js
+++ server.js
@@ -1,74 +1,54 @@
 const express = require("express")
 const crypto = require("crypto")
+const mongoose = require("mongoose")
 
-// const blogPosts = require('./posts.json')
+const postSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  text: { type: String, required: true },
+  liked: { type: Boolean, required: true, default: false }
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 const app = express()
-
-// /posts/7c38b4e289fa
-const blogPosts = [
-  {
-    id: "a2f9d1c49823",
-    name: "Coffee Morning",
-    text: "Tried a new café downtown today, best cappuccino I’ve had in months.",
-    liked: true
-  },
-  {
-    id: "7c38b4e289fa",
-    name: "Late Night Code",
-    text: "Finally fixed that stubborn bug after three hours… victory tastes sweet.",
-    liked: false
-  },
-  {
-    id: "f4b82a7a1e39",
-    name: "Sunday Hike",
-    text: "Went hiking on the Pine Ridge trail. The view from the top was unreal.",
-    liked: true
-  }
-]
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", function (request, response) {
-  response.render("index", { posts: blogPosts })
+app.get("/", async function (request, response) {
+  const allPosts = await Post.find()
+
+  console.log(allPosts)
+
+  response.render("index", { posts: allPosts })
 })
 
 app.get("/posts/create", function (request, response) {
   response.render("create-post")
 })
 
-app.post("/posts/create", function (request, response) {
+app.post("/posts/create", async function (request, response) {
   // title, text
   const newPostValues = request.body
 
-  const newPostId = crypto.randomBytes(6).toString("hex")
-
-  blogPosts.push({
-    id: newPostId,
+  const newPost = await Post.create({
     name: newPostValues.title,
     text: newPostValues.text,
     liked: false
   })
 
-  response.redirect(`/posts/${newPostId}`)
+  response.redirect(`/posts/${newPost.id}`)
 })
 
 // /posts/abc
 // /posts/late-night-code
-app.get("/posts/:postId", function (request, response) {
+app.get("/posts/:postId", async function (request, response) {
   const postId = request.params.postId // 1, 2, 3, a2f9d1c49823
 
   console.log("User visited post", postId)
 
-  let foundPost = null
-
-  for (let post of blogPosts) {
-    if (post.id === postId) {
-      foundPost = post
-    }
-  }
+  let foundPost = await Post.findById(postId)
 
   response.render("post", { post: foundPost })
 })
@@ -90,6 +70,8 @@ app.get("/posts/3", function (request, response) {
   response.render("post", { post: blogPosts[2] })
 })
 */
+
+mongoose.connect("mongodb://localhost:27017/bloggy")
 
 // localhost:3000
 app.listen(3000)
